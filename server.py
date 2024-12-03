2ec180425375548f137e32bcf97d4787e361f46c
carra166 <130591075+carra166@users.noreply.github.com>
2024-12-03 07:54:56 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -10,19 +10,19 @@ error_messages = ['Subject Not Found',
 # Lists for checking validity
 valid_subjects = ('WEATHER', 'NEWS')
 weather_subscribers = []
-#weather_publishers = []
+weather_messages = []
 news_subscribers = []
-#news_publishers = []
+news_messages = []
+associated_lists = {
+    valid_subjects[0] : weather_subscribers,
+    valid_subjects[1] : news_subscribers
+}
 
 
-'''
-:param msg_items: list of items received from client message
-
-'''
+# Checks client sent valid format + action
 def is_valid_message(msg_items, num_items):
     content_type = 'Not Valid'
     action_valid = False
-    subject_exists = False
     if num_items == 1:      # Could be <DISC> or invalid
         if msg_items[0] == 'DISC':
             content_type = 'DISC'
@@ -34,22 +34,31 @@ def is_valid_message(msg_items, num_items):
             action_valid = True
 
     elif num_items == 3:    # Could be <CLIENT_NAME, SUB, SUBJECT> or invalid
-        if (msg_items[1] == 'SUB') and (msg_items[2] in valid_subjects):
+        if msg_items[1] == 'SUB':
             content_type = 'SUB'
             action_valid = True
-            subject_exists = True
 
     elif num_items == 4:    # Could be <CLIENT_NAME, PUB, SUBJECT, MSG> or invalid
-        if (msg_items[1] == 'PUB') and (msg_items[2] in valid_subjects):
+        if msg_items[1] == 'PUB':
             content_type = 'PUB'
             action_valid = True
-            subject_exists = True
 
-    return content_type, action_valid, subject_exists
+    return content_type, action_valid
+
+
+# Checks if a client is subscribed to a subject
+def is_subscribed(subj, client):
+    is_sub = False
+
+    if ((subj == valid_subjects[0]) and (client in associated_lists.get(valid_subjects[0]))) or ((subj == valid_subjects[1]) and (client in associated_lists.get(valid_subjects[1]))):
+        is_sub = True
+
+    return is_sub
 
 
 # Handles each client connection
 def handle_client(client_socket, client_address):
+    client_info = (client_socket, client_address)
     print(f"[INFO] Connected to {client_address}")
     try:
         while True:
@@ -57,38 +66,54 @@ def handle_client(client_socket, client_address):
             message = client_socket.recv(1024).decode("utf-8")
             if not message:
                 break
+
             print(f"[MESSAGE from {client_address}]: {message}")
 
             # Check message for client type, validity
             message_content = [i.strip() for i in message.split(',')]
             message_item_count = len(message_content)
-            msg_type, msg_valid, action_subject = is_valid_message(message_content, message_item_count)
+            msg_type, msg_valid = is_valid_message(message_content, message_item_count)
 
             if msg_valid:
                 if msg_type == 'DISC':
-                    pass
+                    client_socket.sendall("DISC_ACK".encode("utf-8"))
+                    break
+
                 elif msg_type == 'CONN':
-                    pass
+                    client_socket.sendall("CONN_ACK".encode("utf-8"))
+                    continue
+
                 elif msg_type == 'SUB':
-                    # TODO: See errors below to be checked
+                    subject = message_content[2]
+
+                    # Is client attempting to subscribe to non-existent subject?
+                    if subject not in valid_subjects:
+                        client_socket.sendall(f"ERROR: {error_messages[1]}".encode("utf-8"))
+                        continue
+
                     # Is client already subscribed?
-                    # Is client attempting to subscribe to non existent subject?
-                    pass
+                    elif is_subscribed(subject, client_info):
+                        client_socket.sendall(f"ERROR: {error_messages[3]}".encode("utf-8"))
+
+                    else:
+                        client_socket.sendall("SUB_ACK".encode("utf-8"))
+                        # TODO: add to list
+                        continue
+
                 elif msg_type == 'PUB':
                     # TODO: See below for errors to check
                     # Is publisher subscribed to the subject they are publishing?
-                    # Is publisher attempting to publish to a non0existent subject?
+                    # Is publisher attempting to publish to a non-existent subject?
                     pass
 
-            elif () or () or () or ():      # This case covers clients that have been subscribed, but attempted an invalid action
+            # This case covers clients that have been subscribed, but attempted an invalid action
+            elif (client_info in weather_subscribers) or (client_info in news_subscribers):
                 client_socket.sendall(f"ERROR: {error_messages[2]}".encode("utf-8"))
+                continue
 
-            else:       # This case covers a client that is not listed at all, forces it to disconnect
+            # This case covers a client that is not listed at all, forces it to disconnect
+            else:
                 break
-
-            # TODO: Change this accordingly after testing basic functionality
-            # Echo message back
-            client_socket.sendall(f"Server received: {message}".encode("utf-8"))
 
     except Exception as e:
         print(f"[ERROR] {e}")
